24e200a05159e2047ed852037fb8ee8f7b2dc972
74k1 <git.t@betsumei.com>
2026-04-06 16:52:11 +0200

    feat(version): bump 0.1.0 -> 0.1.1

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         let
           snqn = pkgs.vimUtils.buildVimPlugin {
             pname = "snqn.nvim";
-            version = "0.1.0";
+            version = "0.1.1";
             src = self;
           };
         in {
@@ -33,7 +33,7 @@
       legacyPackages = forAllSystems (pkgs: {
         snqn = pkgs.vimUtils.buildVimPlugin {
           pname = "snqn.nvim";
-          version = "0.1.0";
+          version = "0.1.1";
           src = self;
         };
       });
